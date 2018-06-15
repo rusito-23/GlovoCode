b0fa73c132950f774fae0b36246b5093b225b6a1
Rusito23 <i.andruskiewitsch23@gmail.com>
2018-06-14 21:54:18 -0300

    agregando el generador de mails para python3

--- mails.py
+++ mails.py
@@ -1,43 +1,16 @@
-import urllib2
-tagToMatch = 'id="login"'
-valueTag = "value="
-valueSize = len(valueTag) + 1
-yopMailUrl = "http://www.yopmail.com/es/email-generator.php"
-contents = urllib2.urlopen(yopMailUrl).readlines()
-for line in contents :
-	if tagToMatch in line :
-		line = line[line.index(valueTag) + valueSize : -5].replace("&#64;","@")
-		print line
+#Python3
+import urllib.request as get
+import re
+from bs4 import BeautifulSoup
 
+#GET FULL WEBPAGE
+url = 'http://www.yopmail.com/en/email-generator.php'
+content = get.urlopen(url).read()
 
+#PARSE THE TAG
+#TO FIND THE EMAIL
+soup = BeautifulSoup(content, "html.parser")
+attributes = soup.find('input',{'id':'login'}).attrs
+email = attributes['value']
 
-
-'''
-
-{
-	"id":3518135,
-	"type":"Customer",
-	"urn":"glv:customer:548374e2-c96f-4b48-b76c-9a89a92986e3",
-	"name":"beta",
-	"picture":null,
-	"email":"egavetess-0765@yopmail.com",
-	"description":null,
-	"facebookId":null,"
-	preferredCityCode":null,
-	"preferredLanguage":"es",
-	"deviceUrn":null,
-	"analyticsId":null,
-	"mediaCampaign":"Organic Web",
-	"mediaSource":"Organic Web",
-	"os":null,
-	"deliveredOrdersCount":0,
-	"phoneNumber":null,
-	"companyDetail":null,
-	"virtualBalance":{"balance":0.0},
-	"freeOrders":0,
-	"paymentMethod":"CREDIT_CARD",
-	"paymentWay":"PER_ORDER",
-	"currentCard":null,
-	"accumulatedDebt":0.0,
-	"defaulter":false
-}
+print(email)
